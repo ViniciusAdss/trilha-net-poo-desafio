f9667423616cab3b7f1a4bd556a1522e00b28a7d
Vinicius Santos <viniciusads07@gmail.com>
2024-05-27 21:40:34 -0300

    Desafio POO concluido (Vinicius Santos)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11205220", modelo: "Modelo 1", imei:"21515112", memoria: 25);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "6654962", modelo: "Modelo 2", imei:"26201", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
